72c0997d82ae21108245b17e1dfb28c293b01ec5
aalbino221 <aalbino221@gmail.com>
2022-09-03 13:46:05 -0300

    Improvement to Merge Sort speed - 66% increase

--- merge-sort.js
+++ merge-sort.js
@@ -1,25 +1,27 @@
-let array2 = [1,9,5,8,3,6]
+const array2 = [1, 9, 5, 8, 3, 6];
 
 function mergeSort(array) {
-  if (array.length < 2) {return array}
+  if (array.length < 2) return array;
 
-  let arrayOne = mergeSort(array.splice(0,Math.ceil(array.length / 2)));
-  let arrayTwo = mergeSort(array.splice(-Math.ceil(array.length / 2)));
+  const arrayOne = mergeSort(array.splice(0, Math.ceil(array.length / 2)));
+  const arrayTwo = mergeSort(array.splice(-Math.ceil(array.length / 2)));
 
   let helper = [];
 
-  while (arrayOne.length != 0 || arrayTwo.length != 0) {
-    if (arrayOne[0] <= arrayTwo[0]) {
-      helper.push(arrayOne.shift());
-    } else if (arrayOne[0] > arrayTwo[0]) {
-      helper.push(arrayTwo.shift())
-    } else {
-      helper = helper.concat(arrayOne.concat(arrayTwo))
-      break
-    }
+  while (arrayOne.length > 0 || arrayTwo.length > 0) {
+    if (arrayTwo.length == 0) {
+      helper.push(arrayOne[0]);
+      break;
+    } else if (arrayOne.length == 0) {
+      helper.push(arrayTwo[0])
+      break;
+    } else if (arrayOne[0] <= arrayTwo[0]) {
+      helper.push(arrayOne.shift()) 
+    } else helper.push(arrayTwo.shift());
   }
 
   return helper;
 }
 
 mergeSort(array2)
+
