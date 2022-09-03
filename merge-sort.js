e031e1cfb60ebf6015b2f6ce91deac27ca3aa17f
aalbino221 <aalbino221@gmail.com>
2022-09-03 14:31:54 -0300

    Final version

--- merge-sort.js
+++ merge-sort.js
@@ -4,24 +4,17 @@ function mergeSort(array) {
   if (array.length < 2) return array;
 
   const arrayOne = mergeSort(array.splice(0, Math.ceil(array.length / 2)));
-  const arrayTwo = mergeSort(array.splice(-Math.ceil(array.length / 2)));
+  const arrayTwo = mergeSort(array);
 
   let helper = [];
 
-  while (arrayOne.length > 0 || arrayTwo.length > 0) {
-    if (arrayTwo.length == 0) {
-      helper.push(arrayOne[0]);
-      break;
-    } else if (arrayOne.length == 0) {
-      helper.push(arrayTwo[0])
-      break;
-    } else if (arrayOne[0] <= arrayTwo[0]) {
-      helper.push(arrayOne.shift()) 
-    } else helper.push(arrayTwo.shift());
+  while (arrayOne.length != 0 && arrayTwo.length != 0) {
+    arrayOne[0] <= arrayTwo[0] ? helper.push(arrayOne.shift()) : helper.push(arrayTwo.shift());
   }
+
+  arrayOne.length == 0 ? helper.push(arrayTwo[0]) : helper.push(arrayOne[0]);
 
   return helper;
 }
 
 mergeSort(array2)
-
